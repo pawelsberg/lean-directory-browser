b6fdf1333b615e9d242f7432b1819276fa8f1fbf
Pawel Welsberg <pawelsberg@o2.pl>
2024-11-23 23:15:07 +0000

    Bigger refactoring

--- LeanDirectoryBrowser/ProgState/ProgState.lean
+++ LeanDirectoryBrowser/ProgState/ProgState.lean
@@ -16,168 +16,33 @@ inductive ProgState
 deriving Repr, BEq
 
 def isProgStateStart : ProgState → Prop
-  | ProgState.start _ => True
-  | _ => False
-
+| ProgState.start _ => True
+| _ => False
 def isProgStateHeightProvided : ProgState → Prop
 | ProgState.heightProvided _ _ _ _ _ _ _ => True
 | _ => False
-
 def isProgStateFolderBrowser : ProgState → Prop
 | ProgState.folderBrowser _ _ _ _ _ _ _ _ _ _ _ => True
 | _ => False
-
 def isProgStateEmptyFolderBrowser : ProgState → Prop
 | ProgState.emptyFolderBrowser _ _ _ _ _ _ _ => True
 | _ => False
-
 def isProgStateChangingDirectory : ProgState → Prop
   | ProgState.changingDirectory _ _ _ _ _ _ _ => True
   | _ => False
-
 def isProgStateError : ProgState → Prop
 | ProgState.error _ _ => True
 | _ => False
-
 def isProgStateExit : ProgState → Prop
 | ProgState.exit => True
 | _ => False
-
---
 def isProgStateLoading (ps : ProgState) : Prop :=
   match ps with
   | ProgState.start _ => true
   | ProgState.changingDirectory _ _ _ _ _ _ _ => true
   | _ => false
-
--- TODO: why the below is not working to eliminate the match cases but the above does?
-def isProgStateLoading2 (ps : ProgState) : Prop := isProgStateStart ps ∨ isProgStateChangingDirectory ps
-
-def initFonts : List Code :=
-  [
-    Code.storeFont DisplayConstants.displayFileFontFileName DisplayConstants.displayFileFontSize DisplayConstants.displayFileFontStorageName,
-    Code.storeFont DisplayConstants.displayErrorFontFileName DisplayConstants.displayErrorFontSize DisplayConstants.displayErrorFontStorageName,
-    Code.storeFont DisplayConstants.displayHeaderFontFileName DisplayConstants.displayHeaderFontSize DisplayConstants.displayHeaderFontStorageName,
-    Code.run
-  ]
-
-def destroyFonts : List Code :=
-  [
-    Code.destroyStoredFont DisplayConstants.displayFileFontStorageName,
-    Code.destroyStoredFont DisplayConstants.displayErrorFontStorageName,
-    Code.destroyStoredFont DisplayConstants.displayHeaderFontStorageName,
-    Code.run
-  ]
-
-def callCodeProxyWhileWaitingForDisplayWidthAndHeight : List Code :=
-  [
-    Code.clearToColor AllegroColor.black,
-    Code.drawStoredFontStr DisplayConstants.displayErrorFontColour DisplayConstants.displayTopHorizontalMargin DisplayConstants.displayTopVerticalMargin DisplayConstants.displayErrorFontStorageName FontAlignFlags.left "Waiting to receive display width and height"
-  ]
-
-def callCodeProxyToRequestColumnWidth (ps : ProgState) (hPsIsEmpty: isProgStateHeightProvided ps) : List Code :=
+def isProgStateAnyFolderBrowser (ps : ProgState) : Prop :=
   match ps with
-  | ProgState.heightProvided _ _ currentDirectory _ _ _ _ =>
-    let maxStringWidth := match currentDirectory with
-      | File.directory _ (some children) => children.foldl (λ acc f => Nat.max acc (String.length (File.filename f))) 0
-      | _ => DisplayConstants.displayFileDefaultWidth
-    [
-      Code.requestStrWidth DisplayConstants.displayFileFontFileName DisplayConstants.displayFileFontSize (String.join (List.replicate maxStringWidth "W")),
-      Code.run,
-      Code.clearToColor AllegroColor.black,
-      Code.drawStoredFontStr DisplayConstants.displayErrorFontColour DisplayConstants.displayTopHorizontalMargin DisplayConstants.displayTopVerticalMargin DisplayConstants.displayErrorFontStorageName FontAlignFlags.left "String width just requested"
-    ]
-
-def callCodeProxyWhileWaitingForColumnWidth (ps : ProgState) (hPsIsChangingDir: isProgStateChangingDirectory ps) : List Code :=
-  [
-    Code.clearToColor AllegroColor.black,
-    Code.drawStoredFontStr DisplayConstants.displayErrorFontColour DisplayConstants.displayTopHorizontalMargin DisplayConstants.displayTopVerticalMargin DisplayConstants.displayErrorFontStorageName FontAlignFlags.left "String width requested previosly"
-  ]
-
-def callCodeProxyToDrawFolder (ps : ProgState) (hPsIsEmptyFolderBrowserOrFolderBrowser: isProgStateEmptyFolderBrowser ps ∨ isProgStateFolderBrowser ps ) : List Code :=
-  match ps with
-  | ProgState.emptyFolderBrowser _ _ currentDirectory _ _ _ _ =>
-    [
-      Code.clearToColor AllegroColor.black,
-      Code.drawStoredFontStr DisplayConstants.displayHeaderFontColour DisplayConstants.displayTopHorizontalMargin DisplayConstants.displayTopVerticalMargin DisplayConstants.displayHeaderFontStorageName FontAlignFlags.left currentDirectory.path
-    ]
-  | ProgState.folderBrowser root hRootIsDir currentDirectory _ displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
-    let draw_file : File → Nat → Nat → Code
-    | file, x, y =>
-      let filename := file.filename
-      let color := if selectedFilePath == file.path then
-                    DisplayConstants.displayFileSelectedFontColour
-                  else
-                    DisplayConstants.displayFileDeselectedFontColour
-      Code.drawStoredFontStr color x y DisplayConstants.displayFileFontStorageName FontAlignFlags.left filename
-
-    let rec draw_children_one_level : List File → Nat → Nat → List Code
-      | [], _, _ => []
-      | f :: fs, x, y =>
-        (
-          (draw_file f x y)
-          ::
-          (draw_children_one_level fs x (y + DisplayConstants.displayFileFontSize))
-        )
-
-    match currentDirectory, fileOnTopPath with
-      | File.directory _ (some children), fileOnTopPath =>
-        let rec draw_children_columns : List File → Nat → Nat → Nat → List Code
-          | [], _, _, _ => []
-          | fs, n, x, y =>
-            let (to_draw, rest) := fs.splitAt displayRows
-            [
-              draw_children_one_level to_draw x y,
-              if (n > 1) then
-                draw_children_columns rest (n - 1) (x + displayColumnWidth) y
-              else
-                []
-            ].join
-
-        let childrenFromFileOnTop := children.drop (File.indexOfFile children fileOnTopPath)
-        [
-          [
-            Code.clearToColor AllegroColor.black,
-            Code.drawStoredFontStr DisplayConstants.displayHeaderFontColour DisplayConstants.displayTopHorizontalMargin DisplayConstants.displayTopVerticalMargin DisplayConstants.displayHeaderFontStorageName FontAlignFlags.left currentDirectory.path
-          ],
-          draw_children_columns childrenFromFileOnTop displayColumns 0 (DisplayConstants.displayTopVerticalMargin + DisplayConstants.displayHeaderFontSize + DisplayConstants.displayHeaderMargin)
-        ].join
-  | _ => [] -- shouldn't happen
-
-
-  def callCodeProxyWhileInError (ps : ProgState) (hPsInError: isProgStateError ps) : List Code :=
-  match ps with
-  | ProgState.error _ errorMessage =>
-    [
-      Code.clearToColor AllegroColor.black,
-      Code.drawStoredFontStr DisplayConstants.displayErrorFontColour DisplayConstants.displayTopHorizontalMargin DisplayConstants.displayTopVerticalMargin DisplayConstants.displayErrorFontStorageName FontAlignFlags.left errorMessage
-    ]
-  | _ => [] -- shouldn't happen (hPsInError)
-
-
-def generateCodeForProxy (ps prevPs: ProgState) : List Code :=
-  match ps with
-  | ProgState.start currentDirectoryPath =>
-    callCodeProxyWhileWaitingForDisplayWidthAndHeight
-  | ProgState.firstDirectoryLoaded root hRootIsDir =>
-    callCodeProxyWhileWaitingForDisplayWidthAndHeight
-  | ProgState.widthProvided root hRootIsDir displayWidth =>
-    callCodeProxyWhileWaitingForDisplayWidthAndHeight
-  | ProgState.heightProvided root hRootIsDir currentDirectory hCurrentDirectoryIsDir displayWidth displayHeight displayRows =>
-    callCodeProxyToRequestColumnWidth (ProgState.heightProvided root hRootIsDir currentDirectory hCurrentDirectoryIsDir displayWidth displayHeight displayRows) True.intro
-  | ProgState.emptyFolderBrowser root hRootIsDir currentDirectory hCurrentDirectoryIsLoadedEmptyDir displayWidth displayHeight displayRows =>
-    callCodeProxyToDrawFolder (ProgState.emptyFolderBrowser root hRootIsDir currentDirectory hCurrentDirectoryIsLoadedEmptyDir displayWidth displayHeight displayRows) (Or.inl True.intro)
-  | ProgState.folderBrowser root hRootIsDir currentDirectory hCurrentDirectoryIsNonEmptyDirectory displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
-    callCodeProxyToDrawFolder (ProgState.folderBrowser root hRootIsDir currentDirectory hCurrentDirectoryIsNonEmptyDirectory displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath) (Or.inr True.intro)
-  | ProgState.changingDirectory root hRootIsDir currentDirectory hCurrentDirectoryIsDir displayWidth displayHeight displayRows =>
-    callCodeProxyWhileWaitingForColumnWidth (ProgState.changingDirectory root hRootIsDir currentDirectory hCurrentDirectoryIsDir displayWidth displayHeight displayRows) sorry
-  | ProgState.error nextState errorMessage =>
-    callCodeProxyWhileInError (ProgState.error nextState errorMessage) True.intro
-  | ProgState.exit =>
-    []
-
-  def callCodeProxy (ps prevPs: ProgState) (cpp : CodeProxyProcess) : IO Unit := do
-    let codeList : List Code := generateCodeForProxy ps prevPs
-    Code.writeCodeList cpp codeList
-    Code.writeCode cpp Code.run
-    cpp.flush
+  | ProgState.emptyFolderBrowser _ _ _ _ _ _ _ => true
+  | ProgState.folderBrowser _ _ _ _ _ _ _ _ _ _ _ => true
+  | _ => false
--- LeanDirectoryBrowser/ProgState/StateMachine.lean
+++ LeanDirectoryBrowser/ProgState/StateMachine.lean
@@ -4,11 +4,12 @@ import LeanDirectoryBrowser.ProgState.DisplayConstants
 
 namespace ProgState
 
-  def withLoadedChildren (ps : ProgState) (_: isProgStateLoading ps) : IO ProgState :=
+  def withLoadedChildren (ps : ProgState) : IO ProgState :=
     match ps with
     | start rootDirectoryPath => do
       let children ← File.readChildren rootDirectoryPath
-      let currentDirectory := File.directory rootDirectoryPath (some children)
+      let sortedChildren := File.sortFiles children
+      let currentDirectory := File.directory rootDirectoryPath (some sortedChildren)
       pure (firstDirectoryLoaded currentDirectory True.intro)
     | changingDirectory root _ currentDirectory _ displayWidth displayHeight displayRows =>
       match currentDirectory with
@@ -25,11 +26,11 @@ namespace ProgState
                   | [] => pure (emptyFolderBrowser (File.directory newRoot.path newRootChildren) True.intro (File.directory path (some [])) True.intro displayWidth displayHeight displayRows)
                   | _ => pure (heightProvided (File.directory newRoot.path newRootChildren) True.intro loadedCurrentDirectory True.intro displayWidth displayHeight displayRows)
             | _ => pure ps -- shouldn't happen - root should always be a directory
-    | _ => pure ps -- This option shouldn't be needed: compiler doesn't need it but it breaks the program if it's not there! Is it a bug in Lean?
+    | _ => pure ps
 
   def processMovingSelectionForward (ps : ProgState) (_ : isProgStateFolderBrowser ps) (positionsToMove : Nat) : ProgState :=
      match ps with
-     | ProgState.folderBrowser root hRootIsDir currentDirectory hCurrDirIsDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
+     | folderBrowser root hRootIsDir currentDirectory hCurrDirIsDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
         let hcd := hCurrDirIsDir
         match currentDirectory with
         | File.directory _ (some children) =>
@@ -42,11 +43,11 @@ namespace ProgState
              | [] => fileOnTopPath -- shouldn't happen
              | f :: _ => f.path
            else fileOnTopPath
-         ProgState.folderBrowser root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows displayColumns displayColumnWidth newSelectedFilePath newFileOnTopPath
+         folderBrowser root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows displayColumns displayColumnWidth newSelectedFilePath newFileOnTopPath
 
   def processMovingSelectionBackward (ps : ProgState) (_ : isProgStateFolderBrowser ps) (positionsToMove : Nat) : ProgState :=
     match ps with
-    | ProgState.folderBrowser root hRootIsDir currentDirectory hCurrentDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
+    | folderBrowser root hRootIsDir currentDirectory hCurrentDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
       let hcd := hCurrentDir
       match currentDirectory with
       | File.directory _ (some children) =>
@@ -59,28 +60,27 @@ namespace ProgState
             | [] => fileOnTopPath -- shouldn't happen
             | f :: _ => f.path
           else fileOnTopPath
-        ProgState.folderBrowser root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows displayColumns displayColumnWidth newSelectedFilePath newFileOnTopPath
+        folderBrowser root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows displayColumns displayColumnWidth newSelectedFilePath newFileOnTopPath
 
-  -- Returns a new state and a boolean indicating if the input was processed
-  def process (ps : ProgState) (input : String) : ProgState × Bool :=
+  def process (ps : ProgState) (input : String) : ProgState :=
     match ps with
-    | start _ => ⟨ps, false⟩ -- doesn't process - needs IO to load children
+    | start _ => ps
     | firstDirectoryLoaded root hRootIsDir =>
       if input.startsWith "DISPLAY_WIDTH:" then
-        ⟨ProgState.widthProvided root hRootIsDir (input.drop "DISPLAY_WIDTH:".length).toNat!, true⟩
-      else ⟨ps, false⟩
+        widthProvided root hRootIsDir (input.drop "DISPLAY_WIDTH:".length).toNat!
+      else ps
     | widthProvided root hRootIsDir displayWidth =>
       if input.startsWith "DISPLAY_HEIGHT:" then
         let height := (input.drop "DISPLAY_HEIGHT:".length).toNat!
-        ⟨ProgState.heightProvided root hRootIsDir root hRootIsDir displayWidth height ((height - DisplayConstants.displayTopVerticalMargin - DisplayConstants.displayHeaderFontSize - DisplayConstants.displayHeaderMargin) / DisplayConstants.displayFileFontSize),true⟩
-      else ⟨ps, false⟩
+        heightProvided root hRootIsDir root hRootIsDir displayWidth height ((height - DisplayConstants.displayTopVerticalMargin - DisplayConstants.displayHeaderFontSize - DisplayConstants.displayHeaderMargin) / DisplayConstants.displayFileFontSize)
+      else ps
     | heightProvided root hRootIsDir currentDirectory hCurrentDirIsDir displayWidth displayHeight displayRows =>
       let hcd := hCurrentDirIsDir
       match currentDirectory with
       | File.directory _ optionChildren =>
         match optionChildren with
           | some children => match children with
-            | [] => ⟨ProgState.emptyFolderBrowser root hRootIsDir (File.directory currentDirectory.path (some [])) True.intro displayWidth displayHeight displayRows, false⟩
+            | [] => emptyFolderBrowser root hRootIsDir (File.directory currentDirectory.path (some [])) True.intro displayWidth displayHeight displayRows
             | f :: remainingFiles => if input.startsWith "STR_WIDTH:" then
                 let columnWidth := (input.drop "STR_WIDTH:".length).toNat!
                 let displayColumnWidth := (columnWidth + DisplayConstants.displayColumnMargin)
@@ -88,11 +88,11 @@ namespace ProgState
                   | w, cw => (w / cw) -- todo - remove last extra column margin
                 let selectedFilePath := f.path
                 let fileOnTopPath := f.path
-                ⟨ProgState.folderBrowser root hRootIsDir (File.directory currentDirectory.path (some (f :: remainingFiles))) True.intro displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath, true⟩
+                folderBrowser root hRootIsDir (File.directory currentDirectory.path (some (f :: remainingFiles))) True.intro displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath
               else
-                ⟨ps, false⟩ -- only process STR_WIDTH at this stage
-          | none => ⟨ProgState.changingDirectory root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows, false⟩
-      | _ => ⟨ps, false⟩ -- shouldn't happen - currentDirectory should always be a directory
+                ps -- only process STR_WIDTH at this stage
+          | none => changingDirectory root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows
+      | _ => ps -- shouldn't happen - currentDirectory should always be a directory
     | emptyFolderBrowser root hRootIsDir currentDirectory _ displayWidth displayHeight displayRows =>
       match input with
       | "KEY_DOWN:KeyBackspace" =>
@@ -103,35 +103,34 @@ namespace ProgState
             match parentDirectory, displayRows with
             | some (File.directory _ (some children)), displayRows =>
               let newCurrentDirectory := File.directory parentPath (some children)
-              ⟨ProgState.heightProvided root hRootIsDir newCurrentDirectory True.intro displayWidth displayHeight displayRows, true⟩
-            | _, _ => ⟨ps, true⟩ -- shouldn't happen parent directory should always be a directory
-          | _ => ⟨ps, true⟩ -- shouldn't happen - currentDirectory should always be a directory
-      | "KEY_DOWN:KeyQ" => ⟨ProgState.exit, true⟩
-      | "DONE." => ⟨ProgState.exit, true⟩
-      | _ => ⟨ps, false⟩ -- only process KEY_DOWN:KeyBackspace, KEY_DOWN:KeyQ and DONE
+              heightProvided root hRootIsDir newCurrentDirectory True.intro displayWidth displayHeight displayRows
+            | _, _ => ps -- shouldn't happen parent directory should always be a directory
+          | _ => ps -- shouldn't happen - currentDirectory should always be a directory
+      | "KEY_DOWN:KeyQ" => exit
+      | "DONE." => exit
+      | _ => ps -- only process KEY_DOWN:KeyBackspace, KEY_DOWN:KeyQ and DONE
     | folderBrowser root hRootIsDir currentDirectory hCurrentDirIsNonEmptyDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath =>
-      let folderBrowserPs := ProgState.folderBrowser root hRootIsDir currentDirectory hCurrentDirIsNonEmptyDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath
+      let folderBrowserPs := folderBrowser root hRootIsDir currentDirectory hCurrentDirIsNonEmptyDir displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath
       match input with
-      | "KEY_DOWN:KeyDown" => ⟨processMovingSelectionForward folderBrowserPs True.intro 1, true⟩
-      | "KEY_DOWN:KeyRight" => ⟨processMovingSelectionForward folderBrowserPs True.intro displayRows, true⟩
-      | "KEY_DOWN:KeyPageDown" => ⟨processMovingSelectionForward folderBrowserPs True.intro (displayRows * displayColumns), true⟩
-      | "KEY_DOWN:KeyEnd" => ⟨match currentDirectory with
+      | "KEY_DOWN:KeyDown" => processMovingSelectionForward folderBrowserPs True.intro 1
+      | "KEY_DOWN:KeyRight" => processMovingSelectionForward folderBrowserPs True.intro displayRows
+      | "KEY_DOWN:KeyPageDown" => processMovingSelectionForward folderBrowserPs True.intro (displayRows * displayColumns)
+      | "KEY_DOWN:KeyEnd" => match currentDirectory with
         | File.directory _ (some children) => processMovingSelectionForward folderBrowserPs True.intro children.length
-        | _ => ps, true⟩
-      | "KEY_DOWN:KeyUp" => ⟨processMovingSelectionBackward folderBrowserPs True.intro 1, true⟩
-      | "KEY_DOWN:KeyLeft" => ⟨processMovingSelectionBackward folderBrowserPs True.intro displayRows, true⟩
-      | "KEY_DOWN:KeyPageUp" => ⟨processMovingSelectionBackward folderBrowserPs True.intro (displayRows * displayColumns), true⟩
-      | "KEY_DOWN:KeyHome" => ⟨match currentDirectory with
+        | _ => ps
+      | "KEY_DOWN:KeyUp" => processMovingSelectionBackward folderBrowserPs True.intro 1
+      | "KEY_DOWN:KeyLeft" => processMovingSelectionBackward folderBrowserPs True.intro displayRows
+      | "KEY_DOWN:KeyPageUp" => processMovingSelectionBackward folderBrowserPs True.intro (displayRows * displayColumns)
+      | "KEY_DOWN:KeyHome" => match currentDirectory with
         | File.directory _ (some children) => processMovingSelectionBackward folderBrowserPs True.intro children.length
-        | _ => ps, true⟩
+        | _ => ps
       | "KEY_DOWN:KeyEnter" =>
           match currentDirectory with
           | File.directory _ (some children) =>
             match children.find? (λ f => f.path == selectedFilePath) with
             | some (File.directory path children) =>
-              ⟨ProgState.changingDirectory root hRootIsDir (File.directory path children) True.intro displayWidth displayHeight displayRows, true⟩
-            | _ => ⟨ps, true⟩ -- selected file is not a directory - do nothing
-          | _ => ⟨ps, true⟩ -- shouldn't happen - currentDirectory should always be a directory
+              heightProvided root hRootIsDir (File.directory path children) True.intro displayWidth displayHeight displayRows
+            | _ => ps -- selected file is not a directory - do nothing
       | "KEY_DOWN:KeyBackspace" =>
           match currentDirectory with
           | File.directory _ _ =>
@@ -140,36 +139,35 @@ namespace ProgState
             match parentDirectory, displayRows with
             | some (File.directory _ (some children)), displayRows =>
               let newCurrentDirectory := File.directory parentPath (some children)
-              ⟨ProgState.heightProvided root hRootIsDir newCurrentDirectory True.intro displayWidth displayHeight displayRows, true⟩
-            | _, _ => ⟨ps, true⟩ -- shouldn't happen parent directory should always be a directory
-          | _ => ⟨ps, true⟩ -- shouldn't happen - currentDirectory should always be a directory
-      | "KEY_DOWN:KeyQ" => ⟨ProgState.exit, true⟩
-      | "DONE." => ⟨ProgState.exit, true⟩
-      | _ => ⟨ps, false⟩ -- only process KEY_DOWN:KeyDown, KEY_DOWN:KeyRight, KEY_DOWN:KeyPageDown, KEY_DOWN:KeyEnd, KEY_DOWN:KeyUp, KEY_DOWN:KeyLeft, KEY_DOWN:KeyPageUp, KEY_DOWN:KeyHome, KEY_DOWN:KeyBackspace, KEY_DOWN:KeyQ and DONE
+              heightProvided root hRootIsDir newCurrentDirectory True.intro displayWidth displayHeight displayRows
+            | _, _ => ps -- shouldn't happen parent directory should always be a directory
+      | "KEY_DOWN:KeyQ" => exit
+      | "DONE." => exit
+      | _ => ps -- only process KEY_DOWN:KeyDown, KEY_DOWN:KeyRight, KEY_DOWN:KeyPageDown, KEY_DOWN:KeyEnd, KEY_DOWN:KeyUp, KEY_DOWN:KeyLeft, KEY_DOWN:KeyPageUp, KEY_DOWN:KeyHome, KEY_DOWN:KeyBackspace, KEY_DOWN:KeyQ and DONE
     | changingDirectory root hRootIsDir currentDirectory hCurrentDirIsDir displayWidth displayHeight displayRows =>
       let hcd := hCurrentDirIsDir
       match currentDirectory with
       | File.directory _ optionChildren =>
         match optionChildren with
           | some children => match children with
-            | [] => ⟨ProgState.emptyFolderBrowser root hRootIsDir (File.directory currentDirectory.path (some [])) True.intro displayWidth displayHeight displayRows, false⟩
-            | f :: remainingFiles => if input.startsWith "STR_WIDTH:" then
+            | [] => emptyFolderBrowser root hRootIsDir (File.directory currentDirectory.path (some [])) True.intro displayWidth displayHeight displayRows
+            | f :: remainingFiles =>
+              if input.startsWith "STR_WIDTH:" then
                 let columnWidth := (input.drop "STR_WIDTH:".length).toNat!
                 let displayColumnWidth := (columnWidth + DisplayConstants.displayColumnMargin)
                 let displayColumns := match displayWidth, (columnWidth + DisplayConstants.displayColumnMargin) with
                   | w, cw => (w / cw) -- todo - remove last extra column margin
                 let selectedFilePath := f.path
                 let fileOnTopPath := f.path
-                ⟨ProgState.folderBrowser root hRootIsDir (File.directory currentDirectory.path (some (f :: remainingFiles))) True.intro displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath, true⟩
+                folderBrowser root hRootIsDir (File.directory currentDirectory.path (some (f :: remainingFiles))) True.intro displayWidth displayHeight displayRows displayColumns displayColumnWidth selectedFilePath fileOnTopPath
               else
-                ⟨ps, false⟩ -- only process STR_WIDTH at this stage
-          | none => ⟨ProgState.changingDirectory root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows, false⟩
-      | _ => ⟨ps, false⟩ -- shouldn't happen - currentDirectory should always be a directory
+                ps -- only process STR_WIDTH at this stage
+          | none => changingDirectory root hRootIsDir currentDirectory hcd displayWidth displayHeight displayRows
     | error nextState _ =>
       match input with
-      | "KEY_DOWN:KeyEnter" => ⟨nextState, true⟩
-      | "KEY_DOWN:KeyQ" => ⟨ProgState.exit, true⟩
-      | _ => ⟨ps, false⟩ -- only process KEY_DOWN:KeyEnter and KEY_DOWN:KeyQ
-    | exit => ⟨ps, false⟩ -- doesn't process any input
+      | "KEY_DOWN:KeyEnter" => nextState
+      | "KEY_DOWN:KeyQ" => exit
+      | _ => ps -- only process KEY_DOWN:KeyEnter and KEY_DOWN:KeyQ
+    | exit => ps -- doesn't process any input
 
 end ProgState
